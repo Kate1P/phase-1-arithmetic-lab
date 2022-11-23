cb5dfdd3df920223df2fceac554243b5450ba61a
Kate Popova <popova1ea@gmail.com>
2022-11-22 23:50:33 -0800

    commit tree ba7da4b4ca55b4acdf0d31979d21b1212ec648d8

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+    
+    let num1 = 31;
+    let num2 = 2;
+    let multiply = num1*num2;
+    let random = Math.round(Math.random()) + 1;
+    let num3 = 12;
+    let num4 = 3;
+    let mod = num3/num4;
+    let max = Math.max(1, 2, 3, 4, 20);
